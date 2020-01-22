eb51526f6bce453ad9d099aa84b63bf44eed1306
DicPuiWong48 <58582517+DicPuiWong48@users.noreply.github.com>
2020-01-22 09:53:50 +0800

    Create wrapper2.dart

---
+++ project_manager/lib/screens/wrapper2.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:project_manager/models/user.dart';
+import 'package:project_manager/screens/home/home.dart';
+import 'package:project_manager/screens/home2/home2.dart';
+import 'package:provider/provider.dart';
+
+class Wrapper2 extends StatefulWidget {
+  @override
+  _Wrapper2State createState() => _Wrapper2State();
+}
+
+class _Wrapper2State extends State<Wrapper2> {
+  int type;
+  @override
+  Widget build(BuildContext context) {
+    final userData = Provider.of<UserData>(context);
+    type = userData.permissionType;
+
+    if (type == null) {
+      type = 2;
+    }
+    if (type == 1) {
+      return Home();
+    } else if (type == 2) {
+      return Home2();
+    }
+  }
+}
