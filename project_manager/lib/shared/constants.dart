c3ae177534d5c771589a0268f49a1bf04810ff7c
DicPuiWong48 <58582517+DicPuiWong48@users.noreply.github.com>
2020-02-03 15:43:03 +0800

    Update constants.dart

--- project_manager/lib/shared/constants.dart
+++ project_manager/lib/shared/constants.dart
@@ -9,3 +9,13 @@ const textInputDecoration = InputDecoration(
   focusedBorder: OutlineInputBorder(
       borderSide: BorderSide(color: Colors.indigo, width: 2.0)),
 );
+
+const textInputDecoration2 = InputDecoration(
+  isDense: true,
+  fillColor: Colors.white,
+  filled: true,
+  enabledBorder: OutlineInputBorder(
+      borderSide: BorderSide(color: Colors.white, width: 2.0)),
+  focusedBorder: OutlineInputBorder(
+      borderSide: BorderSide(color: Colors.indigo, width: 2.0)),
+);
