1295511d450c3db9fcb5eec2e768d74736f9c6e0
Bratcher1996 <49385342+Bratcher1996@users.noreply.github.com>
2020-02-12 13:19:27 +0800

    Update main.dart

--- project_manager/lib/main.dart
+++ project_manager/lib/main.dart
@@ -1,20 +1,20 @@
-import 'package:flutter/material.dart';
-import 'package:project_manager/models/user.dart';
-import 'package:project_manager/screens/wrapper.dart';
-import 'package:project_manager/services/auth.dart';
-import 'package:provider/provider.dart';
+//File name : main.dart
+//Purpose : root of the app or to start the app
+//Function : Display a splash screen on the phone when user open the app
 
+import 'package:flutter/material.dart';
+import 'package:project_manager/shared/splash_screen.dart';
+
+//main function that will run the app by calling the MyApp function
 void main() => runApp(MyApp());
 
+//MyApp function will return a MaterialApp function which will display SplashScreen() widget when user first open the app
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<User>.value(
-      value: AuthService().user,
-      child: MaterialApp(
-        home: Wrapper(),
-      ),
+    return MaterialApp(
+      home: SplashScreen(),
     );
   }
 }
