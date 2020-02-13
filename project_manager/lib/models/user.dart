d4cc554cddc5f95146d35856870f78219af45d54
Bratcher1996 <49385342+Bratcher1996@users.noreply.github.com>
2020-02-13 09:01:27 +0800

    Update user.dart

--- project_manager/lib/models/user.dart
+++ project_manager/lib/models/user.dart
@@ -1,10 +1,16 @@
+// Name : user.dart
+// Purpose : all the variables that is needed in User class
+// Function : This file contains all the declared and initialised variables for User and UserData class
+
 class User {
   final String uid;
 
   User({this.uid});
 }
 
-class UserData{
+////////////////////////////////////////////////////////////////////////////////
+
+class UserData {
   final String uid;
   final String userName;
   final int permissionType;
