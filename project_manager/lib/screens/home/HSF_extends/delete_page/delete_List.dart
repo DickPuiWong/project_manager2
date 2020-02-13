c017b4c714e04902962b25e09390ddb761a6f8a2
Bratcher1996 <49385342+Bratcher1996@users.noreply.github.com>
2020-02-13 09:18:29 +0800

    Update delete_List.dart

--- project_manager/lib/screens/home/HSF_extends/delete_page/delete_List.dart
+++ project_manager/lib/screens/home/HSF_extends/delete_page/delete_List.dart
@@ -1,3 +1,7 @@
+// Name : delete_List.dart
+// Purpose : To enable user delete any project that has been created
+// Function : This class will build the project provider and then return DeleteTile class
+
 import 'package:flutter/material.dart';
 import 'package:project_manager/models/Project.dart';
 import 'package:project_manager/screens/home/HSF_extends/delete_page/delete_tiles.dart';
@@ -11,12 +15,17 @@ class DeleteList extends StatefulWidget {
 class _DeleteListState extends State<DeleteList> {
   @override
   Widget build(BuildContext context) {
+    //creation of the projects object and assigned it to provider of List of the Project class
     final projects = Provider.of<List<Project>>(context);
 
+    //return the ListView builder widget and return the DeleteTile class
     return ListView.builder(
       itemCount: projects.length,
       itemBuilder: (context, index) {
-        return DeleteTile(proj: projects[index],num: index,);
+        return DeleteTile(
+          proj: projects[index],
+          num: index,
+        );
       },
     );
   }
