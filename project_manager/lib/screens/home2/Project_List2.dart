3a810340aafbf5b03c15ab8cb9f79e45ba86cb8c
Bratcher1996 <49385342+Bratcher1996@users.noreply.github.com>
2020-02-13 10:48:58 +0800

    Update Project_List2.dart

--- project_manager/lib/screens/home2/Project_List2.dart
+++ project_manager/lib/screens/home2/Project_List2.dart
@@ -1,3 +1,7 @@
+// Name : Project_List2.dart
+// Purpose : to create or build a list of projects
+// Function : This page will build a list of existing projects with all their details only for supervisor permission type
+
 import 'package:flutter/material.dart';
 import 'package:project_manager/models/Project.dart';
 import 'package:project_manager/models/user.dart';
@@ -14,11 +18,11 @@ class _ProjList2State extends State<ProjList2> {
   Widget build(BuildContext context) {
     final projects = Provider.of<List<Project>>(context);
     final userData = Provider.of<UserData>(context);
-    List<Project> userProjList=[];
+    List<Project> userProjList = [];
 
-    for(int i=0;i<userData.projList.length;i++){
-      for(int ii=0;ii<projects.length;ii++){
-        if(projects[ii].projID == userData.projList[i]){
+    for (int i = 0; i < userData.projList.length; i++) {
+      for (int ii = 0; ii < projects.length; ii++) {
+        if (projects[ii].projID == userData.projList[i]) {
           Project temp = projects[ii];
           userProjList.add(temp);
         }
