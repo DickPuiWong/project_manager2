419f4cdb76e7c7f2b2be82115d7ae348ccf3cc90
Bratcher1996 <49385342+Bratcher1996@users.noreply.github.com>
2020-02-18 14:04:15 +0800

    Update Project.dart

--- project_manager/lib/models/Project.dart
+++ project_manager/lib/models/Project.dart
@@ -4,20 +4,14 @@
 
 class Project {
   final String projID;
-  final double budget;
-  final double spentBudget;
   final String projname;
   final String location;
-  final double completion;
   final double adhesiveTotalLitre;
   final double adhesiveUsedLitre;
-  final double adhesivePrice;
   final double abrasiveTotalWeight;
   final double abrasiveUsedWeight;
-  final double abrasivePrice;
   final double paintTotalLitre;
   final double paintUsedLitre;
-  final double paintPrice;
   final double totalSurfaceAreaB;
   final double blastedArea;
   final double totalSurfaceAreaP;
@@ -32,20 +26,14 @@ class Project {
 
   Project({
     this.projID,
-    this.budget,
-    this.spentBudget,
     this.projname,
     this.location,
-    this.completion,
     this.adhesiveTotalLitre,
     this.adhesiveUsedLitre,
-    this.adhesivePrice,
     this.abrasiveTotalWeight,
     this.abrasiveUsedWeight,
-    this.abrasivePrice,
     this.paintTotalLitre,
     this.paintUsedLitre,
-    this.paintPrice,
     this.totalSurfaceAreaB,
     this.blastedArea,
     this.totalSurfaceAreaP,
@@ -66,13 +54,13 @@ class BlastPot {
   final int num;
   final double usedAbrasive;
   final double usedAdhesive;
-  final double usedPaint;
+  final double usedHours;
 
   BlastPot({
     this.num,
     this.usedAbrasive,
     this.usedAdhesive,
-    this.usedPaint,
+    this.usedHours,
   });
 }
 
