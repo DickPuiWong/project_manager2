d94265bcd5f2ebf7b1c9b67e5b600f734ea7b458
DicPuiWong48 <58582517+DicPuiWong48@users.noreply.github.com>
2020-02-20 13:52:02 +0800

    Update project_details.dart

--- project_manager/lib/shared/Details/project_details.dart
+++ project_manager/lib/shared/Details/project_details.dart
@@ -612,7 +612,7 @@ class _PDExtendState extends State<PDExtend> {
                     ),
                     subtitle: Center(
                         child: Column(
-                      children: findOtherAsigned(),
+                      children: findOtherAssigned(),
                     )),
                   ),
                   Container(
