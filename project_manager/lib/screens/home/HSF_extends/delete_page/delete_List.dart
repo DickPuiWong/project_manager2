630915cfde58fa18b5c21cdc15f1fe79b6e99e43
DicPuiWong48 <58582517+DicPuiWong48@users.noreply.github.com>
2020-02-21 10:39:26 +0800

    Update delete_List.dart

--- project_manager/lib/screens/home/HSF_extends/delete_page/delete_List.dart
+++ project_manager/lib/screens/home/HSF_extends/delete_page/delete_List.dart
@@ -4,6 +4,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:project_manager/models/Project.dart';
+import 'package:project_manager/models/user.dart';
 import 'package:project_manager/screens/home/HSF_extends/delete_page/delete_tiles.dart';
 import 'package:provider/provider.dart';
 
@@ -17,13 +18,29 @@ class _DeleteListState extends State<DeleteList> {
   Widget build(BuildContext context) {
     //creation of the projects object and assigned it to provider of List of the Project class
     final projects = Provider.of<List<Project>>(context);
+    final users = Provider.of<List<UserData>>(context);
+    List<Project> orderByDate = [];
+
+    for (int i = 0; i < projects.length; i++) {
+      orderByDate.add(projects[i]);
+      for (int ii = 1; ii < orderByDate.length; ii++) {
+        if (orderByDate[i].date < orderByDate[ii - 1].date) {
+          Project temp = orderByDate[ii - 1];
+          orderByDate[ii - 1] = orderByDate[i];
+          orderByDate[i] = temp;
+        }
+      }
+    }
+
+
 
     //return the ListView builder widget and return the DeleteTile class
     return ListView.builder(
-      itemCount: projects.length,
+      itemCount: orderByDate.length,
       itemBuilder: (context, index) {
         return DeleteTile(
-          proj: projects[index],
+          users: users,
+          proj: orderByDate[index],
           num: index,
         );
       },
