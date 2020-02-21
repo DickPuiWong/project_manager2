554d9d0f70d048a8f0bcd24eaf9d652a8a32f06f
DicPuiWong48 <58582517+DicPuiWong48@users.noreply.github.com>
2020-02-21 14:09:52 +0800

    Update BlastPotData.dart

--- project_manager/lib/models/BlastPotData.dart
+++ project_manager/lib/models/BlastPotData.dart
@@ -1,6 +1,8 @@
 class BlastPotDetails{
+  final String id;
+  final int num;
   final double totalUsedHours;
-  final double limit;
+  final double limit=1000;
 
-  BlastPotDetails({this.totalUsedHours,this.limit});
+  BlastPotDetails({this.id,this.num,this.totalUsedHours});
 }
